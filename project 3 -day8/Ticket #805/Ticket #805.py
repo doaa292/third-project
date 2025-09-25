448774cde7b74ca6f2a5e5320c5db5e9b79bfcef
doaa292 <doaa.mansour@learner.42.tech>
2025-09-25 05:52:16 +0300

    Add files via upload

---
+++ project 3 -day8/Ticket #805/Ticket #805.py
@@ -0,0 +1,3 @@
+import winsound
+
+winsound.PlaySound('sound1.mp3', winsound.SND_ALIAS | winsound.SND_ASYNC)
---
+++ project 3 -day8/Ticket#801/Ticket#801.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Style, Back
+print(Fore. GREEN + "INFO: Task OK" + Style. RESET_ALL)
+print(Back.YELLOW + "INFO: Task OK" + Style. RESET_ALL)
+print(Fore.RED + "ERROR: Something failed" +
+Style. RESET_ALL)
